4d3dd7fd3bf2048f851e75077a40188d81739955
aman-ahluwalia <aman.explorer@gmail.com>
2017-12-23 15:50:45 +0530

    Calculate speed on track dependent on surface type

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -3,6 +3,15 @@ module Api
     class CarsController < ApplicationController
       def show
         @car = Car.find_by(slug: params[:id].to_s)
+        @max_speed_on_track = 'no track selected'
+        if params[:track].present?
+          @track = Track.find_by('lower(name) = ?', params[:track].downcase)
+          if @track.present?
+            @max_speed_on_track = (@car.max_speed.to_f - (@car.max_speed.to_f * @track.factor.resistance / 100)).to_s + @car.speed_unit
+          else
+            @max_speed_on_track = 'track not found'
+          end
+        end
       end
     end
   end
--- app/views/api/v1/cars/show.json.jbuilder
+++ app/views/api/v1/cars/show.json.jbuilder
@@ -2,5 +2,5 @@ json.car do
   json.id                   @car.id
   json.car_slug             @car.slug
   json.max_speed            @car.max_speed.to_s + @car.speed_unit
-  json.max_speed_on_track   "no track selected"
+  json.max_speed_on_track   @max_speed_on_track
 end
